9449cc2e5fa79b346a5856679c9b29d99c898a72
Akiane-cloudy <josker6969@gmail.com>
2024-05-24 15:01:42 +0800

    Update: 更新控制功能變成可接收Kinect來的控制json

---
+++ Controller.py
@@ -0,0 +1,24 @@
+import asyncio
+import websockets
+import json
+import pyautogui
+
+async def echo(websocket, path):
+    with open('./key_bindings.json', 'r') as file:
+        keyBoardSettingJson = json.load(file) # get keyboard setting from json
+        async for message in websocket:
+            data = json.loads(message)
+            for key in data:
+                if data[key] == True:
+                    print("Received message:", key)
+                    pyautogui.press(keyBoardSettingJson[key])
+                    print("Output keyBoard signal :" + keyBoardSettingJson[key])
+
+async def main():
+
+    async with websockets.serve(echo, "localhost", 8765):
+        print("Controller Listening...")
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
