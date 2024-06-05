f2a679443f003665a36bd560b3388c453a1e2aff
Akiane-cloudy <josker6969@gmail.com>
2024-06-05 23:22:18 +0800

    Modify: modify

--- Controller.py
+++ Controller.py
@@ -2,9 +2,11 @@ import asyncio
 import websockets
 import json
 import pyautogui
+import pydirectinput
 import subprocess
 import time
 
+pydirectinput.PAUSE = 0.0000001
 pyautogui.PAUSE = 0.0000001
 stopEvent = asyncio.Event()
 process = None
@@ -12,19 +14,22 @@ process = None
 async def echo(websocket, path):
     try:
         print(f"Client connected from {websocket.remote_address}")
-        with open("./key_bindings.json", "r") as file:
-            keyBoardSettingJson = json.load(file)  # get keyboard setting from json
+        with open('./key_bindings.json', 'r') as file:
+            keyBoardSettingJson = json.load(file) # get keyboard setting from json
             async for message in websocket:
                 data = json.loads(message)
                 for key in data:
                     if data[key] == True:
-                        if key != "":
-                            print("Received message:", key)
-                            pyautogui.press(keyBoardSettingJson[key])
+                        print("Received message:", key)
+                        pydirectinput.press(keyBoardSettingJson[key])
+                        # pyautogui.press(keyBoardSettingJson[key])
+                        # pyautogui.keyDown(keyBoardSettingJson[key])
+                    else:
+                        pydirectinput.keyUp(keyBoardSettingJson[key])
+                        # pyautogui.keyUp(keyBoardSettingJson[key])
     except websockets.exceptions.ConnectionClosedError as e:
         print(f"Connection closed with error: {e}")
         stopEvent.set()
-
 
 async def main():
     async with websockets.serve(echo, "localhost", 8765):
@@ -34,13 +39,11 @@ async def main():
         await stopEvent.wait()  # run until there has no signal input
         process.terminate()
 
-
 def closeKinect():
     global process
     if process is not None:
         process.terminate()
         process = None
-
 
 if __name__ == "__main__":
     try:
